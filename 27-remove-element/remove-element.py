2510432bb40bee26ea08666da55cd75dc514bd12
Noor Athirah <88584117+NoorAthirah@users.noreply.github.com>
2025-07-23 08:19:01 +0800

    Time: 0 ms (100.00%) | Memory: 12.6 MB (9.19%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -5,14 +5,12 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        result = []
+        k=0 
 
         for i in range(len(nums)):
             if nums[i] != val:
-                result.append(nums[i])
-        
-        for i in range(len(result)):
-            nums[i] = result[i]
-        
-        return(len(result))
+                nums[k] = nums[i]
+                k +=1
+        return k
+                
         
